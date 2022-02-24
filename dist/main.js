1b34910574da25781e5d571e57a15a19544ee945
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-24 13:45:22 -0500

    understanding interface

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+// const getFullName = (name: string, surname: string): string => {
+// 	return name + ' ' + surname;
+// };
+var user = {
+    name: 'Sam',
+    age: 30
+};
+var user2 = {
+    name: 'Sam',
+    age: 30
+};
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+// const getFullName = (name: string, surname: string): string => {
+// 	return name + ' ' + surname;
+// };
+
+// console.log(getFullName('Sam', 'Bencivengo'));
+interface User {
+	name: string;
+	age?: number;
+}
+
+const user: User = {
+	name: 'Sam',
+	age: 30,
+};
+
+const user2: User = {
+	name: 'Sam',
+	age: 30,
+};
+
+
